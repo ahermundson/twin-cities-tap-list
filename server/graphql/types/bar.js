16cc388a1d30851c97fdeceea70f7a9ad127b43f
Alex Hermundson <alex.hermundson@gmail.com>
2017-07-11 21:56:15 -0500

    adding graphQL types

---
+++ server/graphql/types/bar.js
@@ -0,0 +1,54 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLInt
+} from 'graphql';
+
+import BarModel from '../../models/bar-model'
+import { beerType } from './beer'
+
+export const barType = new GraphQLObjectType({
+  name: 'Bar',
+  description: 'Bar API',
+  fields: () => ({
+    _id: {
+      type: new GraphQLNonNull(GraphQLID);
+    },
+    bar_name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    street_address: {
+      type: GraphQLString
+    },
+    city: {
+      type: GraphQLString
+    },
+    state: {
+      type: GraphQLString
+    },
+    zip: {
+      type: GraphQLInt
+    },
+    latitude: {
+      type: GraphQLInt
+    },
+    longitude: {
+      type: GraphQLInt
+    },
+    beers_on_tap: {
+      type: new GraphQLList(beerType),
+      resolve(beer) {
+        const { _id } = beer;
+        return BeerModel.find({beers_on_tap: _id}).exec();
+      },
+      website: {
+        type: {
+          GraphQLString
+        }
+      }
+    }
+  })
+});
---
+++ server/graphql/types/beer.js
@@ -0,0 +1,30 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLID
+} from 'graphql'
+
+import BeerModel from '../../models/beer-model'
+import { breweryType } from './brewery'
+
+export const beerType = new GraphQLObjectType({
+  name: 'Beer',
+  description: 'Beer API',
+  fields: () => ({
+    _id: {
+      type: new GraphQLNonNull(GraphQLID)
+    },
+    beer_name: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    style: {
+      type: GraphQLString
+    },
+    {
+      brewery_name: {
+        type: GraphQLID
+      }
+    }
+  })
+});
